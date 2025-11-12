a87edc3cff7eea076d7aaca9b01ccc72d60882ba
Eli Boyarski <eli@boyar.ski>
2025-11-12 09:15:07 +0200

    A working version, without skill weights

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from typing import Optional
+from typing import Optional, Any
 
 from superlinked import framework as sl
 
@@ -15,8 +15,7 @@ class OpenPosition(sl.Schema):
     description: sl.String
     allocated_pay: sl.Integer  # In annual USD
     required_date_of_filling: sl.Timestamp
-    required_skills: sl.StringList
-    required_skill_weights: sl.FloatList
+    skill_weights: sl.FloatList  # All skills weighted, ordered by id
     # No years of experience! It's a substitue for skill proficiency
     # No academic background field, for same reason
     created_at: sl.Timestamp
@@ -33,18 +32,10 @@ class Candidate(sl.Schema):
     # Relevant details
     desired_pay: sl.Integer  # In annual USD
     self_description: sl.String
-    date_of_availability: sl.Timestamp
+    date_of_availability: sl.Integer  # int(datetime.timestamp())
     # No age, gender, or photo fields to avoid bias
-    created_at: sl.Timestamp
-    updated_at: sl.Timestamp
-
-
-class CandidateEvaluation(sl.Schema):
-    id: sl.IdField  # pyright: ignore[reportIncompatibleMethodOverride]
-    candidate_id: sl.String  # Candidate being evaluated
-    position_id: sl.String  # Position being applied for
-    evaluated_skills: sl.StringList
-    evaluated_skill_scores: sl.FloatList
+    # Evaluation details
+    skill_scores: Optional[sl.FloatList]  # Evaluated skill scores, ordered by skill id
     interviewer_name: sl.String
     created_at: sl.Timestamp
     updated_at: sl.Timestamp
@@ -54,13 +45,45 @@ def main():
     skill = Skill()
     open_position = OpenPosition()
     candidate = Candidate()
-    candidate_evaluation = CandidateEvaluation()
+
     skill_source: sl.InMemorySource = sl.InMemorySource(skill)
     position_source: sl.InMemorySource = sl.InMemorySource(open_position)
     candidate_source: sl.InMemorySource = sl.InMemorySource(candidate)
-    candidate_evaluation_source: sl.InMemorySource = sl.InMemorySource(candidate_evaluation)
 
-    executor = sl.InMemoryExecutor(sources=[skill_source, position_source, candidate_source, candidate_evaluation_source], indices=[])
+    now = int(datetime(2025, 11, 12).timestamp())
+
+    frontend_position_dict: dict[str, Any] = dict(
+        id="position1",
+        title="Senior Frontend Developer",
+        description="We are looking for a skilled frontend developer to join our team. Must be proficient in JavaScript, React, and CSS.",
+        allocated_pay=120_000,
+        required_date_of_filling=int(datetime(2025, 12, 15).timestamp()),
+        skill_weights=[
+            0.2,  # Team player
+            0.5,  # Frontend technologies
+            0.3,  # Culture fit
+            0.0,  # Leadership
+        ],
+        created_at=now,
+        updated_at=now,
+    )
+
+    # Candidate spaces and index
+    description_space = sl.TextSimilaritySpace(text=candidate.self_description, model="sentence-transformers/all-mpnet-base-v2")
+    desired_pay_space = sl.NumberSpace(number=candidate.desired_pay, mode=sl.Mode.MINIMUM, min_value=frontend_position_dict["allocated_pay"], max_value=300_000)
+    availability_space = sl.NumberSpace(number=candidate.date_of_availability, mode=sl.Mode.MINIMUM,
+                                        min_value=now, max_value=frontend_position_dict["required_date_of_filling"])
+    skill_scores_space = sl.CustomSpace(vector=candidate.skill_scores, length=4)
+    candidate_index = sl.Index(
+        spaces=[
+            description_space,
+            desired_pay_space,
+            availability_space,
+            skill_scores_space
+        ],
+    )
+
+    executor = sl.InMemoryExecutor(sources=[skill_source, position_source, candidate_source], indices=[candidate_index])
     app = executor.run()
 
     skill_source.put([
@@ -71,25 +94,7 @@ def main():
     ])
 
     position_source.put([
-        dict(
-            id="position1",
-            title="Senior Frontend Developer",
-            description="We are looking for a skilled frontend developer to join our team. Must be proficient in JavaScript, React, and CSS.",
-            allocated_pay=120000,
-            required_date_of_filling=datetime(2025, 12, 1),
-            required_skills=[
-                "skill1",  # Team player
-                "skill2",  # Frontend technologies
-                "skill3",  # Culture fit
-            ],
-            required_skill_weights=[
-                0.2,  # Team player
-                0.5,  # Frontend technologies
-                0.3,  # Culture fit
-            ],
-            created_at=datetime.now(),
-            updated_at=datetime.now(),
-        ),
+        frontend_position_dict
     ])
 
     candidate_source.put([
@@ -100,9 +105,16 @@ def main():
             phone_number="555-1234",
             desired_pay=115000,
             self_description="Experienced frontend developer with a passion for creating user-friendly web applications.",
-            date_of_availability=datetime(2025, 11, 15),
-            created_at=datetime.now(),
-            updated_at=datetime.now(),
+            date_of_availability=int(datetime(2025, 11, 15).timestamp()),
+            skill_scores=[
+                7,  # Team player
+                10,  # Frontend technologies
+                7,  # Culture fit
+                -1,  # Leadership (not evaluated)
+            ],
+            interviewer_name="Carol",
+            created_at=now,
+            updated_at=now,
         ),
         dict(
             id="candidate2",
@@ -111,50 +123,38 @@ def main():
             phone_number="555-5678",
             desired_pay=125000,
             self_description="Seasoned developer with expertise in React and a strong focus on team collaboration.",
-            date_of_availability=datetime(2025, 12, 5),
-            created_at=datetime.now(),
-            updated_at=datetime.now(),
+            date_of_availability=int(datetime(2025, 12, 5).timestamp()),
+            skill_scores=[
+                7,  # Team player
+                8,  # Frontend technologies
+                10,  # Culture fit
+                -1,  # Leadership (not evaluated)
+            ],
+            interviewer_name="Dave",
+            created_at=now,
+            updated_at=now,
         ),
     ])
 
-    candidate_evaluation_source.put([
-        dict(
-            id="eval1",
-            candidate_id="candidate1",
-            position_id="position1",
-            evaluated_skills=[
-                "skill1",  # Team player
-                "skill2",  # Frontend technologies
-                "skill3",  # Culture fit
-            ],
-            evaluated_skill_scores=[
-                8,  # Team player
-                9,  # Frontend technologies
-                7,  # Culture fit
-            ],
-            interviewer_name="Carol",
-            created_at=datetime.now(),
-            updated_at=datetime.now(),
-        ),
-        dict(
-            id="eval2",
-            candidate_id="candidate2",
-            position_id="position1",
-            evaluated_skills=[
-                "skill1",  # Team player
-                "skill2",  # Frontend technologies
-                "skill3",  # Culture fit
-            ],
-            evaluated_skill_scores=[
-                7,  # Team player
-                8,  # Frontend technologies
-                9,  # Culture fit
-            ],
-            interviewer_name="Dave",
-            created_at=datetime.now(),
-            updated_at=datetime.now(),
-        ),
-    ])
+    query = (
+        sl.Query(
+            candidate_index,
+            weights={
+                description_space: sl.Param("description_similarity_weight", default=0.03),
+                skill_scores_space: sl.Param("skill_scores_weight", default=0.9),
+                desired_pay_space: sl.Param("desired_pay_weight", default=0.03),
+                availability_space: sl.Param("availability_weight", default=0.04),
+            }
+        )
+        .find(candidate)
+        .similar(skill_scores_space, [10.0, 10.0, 10.0, -1.0])  # weight=frontend_position_dict["skill_weights"] can't provide dynamic weights to CustomSpace :(
+        .similar(description_space, frontend_position_dict["description"])
+        # desired pay similarity and availability similarity are implicitly handled by NumberSpace with MINIMUM mode
+        .select_all()
+    )
+    result = app.query(query)
+    df = sl.PandasConverter.to_pandas(result)
+    print(df)
 
 
 if __name__ == "__main__":
